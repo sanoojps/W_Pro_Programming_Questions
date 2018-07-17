a4ad6ce750a511e91b2f0e9d042ca999fdb4344a
Sanooj <avowalscribe@gmail.com>
2018-07-17 21:19:11 +0530

    Initial Commit

---
+++ W_Pro_Programming_Questions/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W_Pro_Programming_Questions
+//
+//  Created by Sanooj on 17/07/2018.
+//  Copyright © 2018 Sanooj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
